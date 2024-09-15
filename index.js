26dcbbcd0b47f3194041be39e70bc22d59c227da
Md. Rabiul Alam <mavenrabiul@gmail.com>
2024-09-16 01:11:26 +0600

    Question no. 3 solved
    Question no. 3 solved

--- index.js
+++ index.js
@@ -5,11 +5,23 @@ function calculateDifference(a, b) {return a - b;}
 let output = calculateDifference(100, 5);
 console.log(output);
 
+
 /*2) Write a function named isOdd that takes one argument and returns true if the number is odd, and false if it is not.  */
 
 
 function isOdd(number) {
     return number % 2 !== 0
 }
-console.log(isOdd(1));
-console.log(isOdd(2));
+console.log(isOdd(1)); //true
+console.log(isOdd(2)); //false
+
+/* 3) Write a function named findMin that takes an array of numbers and returns the smallest number in the array. */
+
+function findMin(numbers){
+    return Math.min(...numbers);
+
+}
+let numbers = [85,100,25,32,76,55,59,11];
+console.log(findMin(numbers));
+
+
