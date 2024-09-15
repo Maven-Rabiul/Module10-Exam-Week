881b7f1893de123d9f0e3bfb9cafb43407232630
Md. Rabiul Alam <mavenrabiul@gmail.com>
2024-09-16 00:48:24 +0600

    1st question solved

---
+++ index.js
@@ -0,0 +1,6 @@
+
+/*Write a function named calculateDifference that takes two arguments and returns the difference between the first and second argument.*/
+
+function calculateDifference(a, b) {return a - b;}
+let output = calculateDifference(100, 5);
+console.log(output);
